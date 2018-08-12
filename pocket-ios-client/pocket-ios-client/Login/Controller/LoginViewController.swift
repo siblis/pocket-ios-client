0a8564157a68241530172863cd9b20156f158ec0
vladimirdamien <damien.gabber@gmail.com>
2018-08-12 22:50:25 +0300

    Начало приложения Скелет для приложения Начал разрабатывать систему логирования

---
+++ pocket-ios-client/pocket-ios-client/Login/Controller/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pocket-ios-client
+//
+//  Created by Damien on 12.08.2018.
+//  Copyright © 2018 Damien Inc. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
