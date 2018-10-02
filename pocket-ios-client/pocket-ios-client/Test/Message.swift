24d03a1cadca828fac1f99d49e3f0968ab7503db
vladimirdamien <damien.gabber@gmail.com>
2018-10-02 15:31:37 +0300

    В Папке - Test сделано взаимодействие с сервером: - Логирование - Получения иформации о себе - Коннект с сервером через WS - Внедрена библиотека для  WS - Starscream

---
+++ pocket-ios-client/pocket-ios-client/Test/Message.swift
@@ -0,0 +1,16 @@
+//
+//  message.swift
+//  pocket-ios-client
+//
+//  Created by Damien on 01/10/2018.
+//  Copyright © 2018 Damien Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Message: Codable {
+    
+    var receiver: Int
+    var message: String
+    
+}
---
+++ pocket-ios-client/pocket-ios-client/Test/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  pocket-ios-client
+//
+//  Created by Damien on 30/09/2018.
+//  Copyright © 2018 Damien Inc. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    let account_name: String
+    let password: String
+    
+}
